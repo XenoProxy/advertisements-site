6c745ef48a8cd456028f22ecc2dcf95703b9adcc
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2021-09-08 13:18:32 +0300

    [+] User model

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class AdvUser(AbstractUser):
+    is_avtivated = models.BooleanField(
+        default=True,
+        db_index=True,
+        verbose_name='Is activated?',
+    )
+    send_messages = models.BooleanField(
+        default=True,
+        verbose_name='Send messages about new comments?',
+    )
+
+    class Meta(AbstractUser.Meta):
+        pass
